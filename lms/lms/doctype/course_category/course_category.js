48f288a25a7b0707bab3c423472b50e9403ed63a
Mohammed Almatrafi <almatrafimfq@marafiq.com.sa>
2026-06-09 13:43:29 +0300

    feat: add lms specific doctypes

---
+++ lms/lms/doctype/course_category/course_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Mohammed Almatrafi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Course Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
